06702b6e7863350a36dc7d31ddf8f20c7c3170e4
Carlton Aikins <caikins317@gmail.com>
2023-11-05 01:17:59 -0800

    Update controller.ts

--- src/api/v1/auth/controllers.ts
+++ src/api/v1/auth/controllers.ts
@@ -1,12 +1,63 @@
-import Elysia from "elysia";
-
+import { Elysia, t } from 'elysia';
+import { supabase } from '../../../libs/supabase';
+// import { cookie } from '@elysiajs/cookie';
 
 export const authen = (app: Elysia) =>
-    app.group('/auth', (app) =>
-        app.post('/signup', () => {
-            return "expected signup";
+  app.group(
+    '/auth',
+    (app) =>
+      app
+        .setModel({
+          sign: t.Object({
+            email: t.String({
+              format: 'email',
+            }),
+            password: t.String({
+              minLength: 8,
+            }),
+          }),
         })
-        .post('/login', () => {
-            return 'exepcted login';
-        })
-    )
+        .post(
+          '/signup',
+          async ({ body }) => {
+            const { data, error } = await supabase.auth.signUp(body);
+
+            if (error) return error;
+
+            return data.user;
+          },
+          {
+            schema: {
+              body: 'sign',
+            },
+          }
+        )
+        .post(
+          '/login',
+          async ({ body }) => {
+            const { data, error } = await supabase.auth.signInWithPassword(
+              body
+            );
+
+            if (error) return error;
+
+            return data.user;
+          },
+          {
+            schema: {
+              body: 'sign',
+            },
+          }
+        )
+    //   .get('/refresh', async ({ setCookie, cookie: { refresh_token } }) => {
+    //     const { data, error } = await supabase.auth.refreshSession({
+    //       refresh_token,
+    //     });
+
+    //     if (error) return error;
+
+    //     setCookie('refresh_token', data.session!.refresh_token);
+
+    //     return data.user;
+    //   })
+  );
